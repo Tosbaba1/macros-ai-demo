5c1a05acbbb1943fb991b871d01f44128ce53db7
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-04-28 19:11:33 +0000

    Fetch params for image and description

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -2,4 +2,11 @@ class FoodController < ApplicationController
   def estimate
     render ({ :template => "macros/calculate" })
   end
+
+  def results
+    @the_image = params.fetch("image_param")
+    @the_description = params.fetch("description_param")
+
+    render ({ :template => "macros/results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get("/blank_form", { :controller => "food", :action => "estimate" })
 
+  get("/macro_results", { :controller => "food", :action => "results" })
+
   # Solutions below. Don't peek until you try it yourself and get stuck!
 
   get("/solutions/blank_form", { :controller => "solutions", :action => "display_form" })
