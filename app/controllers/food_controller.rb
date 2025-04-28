9a2c942d0011f610d7e19f284c58727cb573575b
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-04-28 19:58:40 +0000

    Extracting Macros for AI response

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -10,41 +10,46 @@ class FoodController < ApplicationController
     c = OpenAI::Chat.new
     c.system ("You are a nutrisionist that speaks like Gordon Ramsey. The user will give you an image and/or a description of a meal. Your job is to estimate the macronutrients and calories in it.")
 
-    c.user("How many calories in a slice of pizza?")
+    c.user(@the_description)
 
     c.schema = '{
-  "name": "nutritional_info",
-  "schema": {
-    "type": "object",
-    "properties": {
-      "carbohydrates": {
-        "type": "number",
-        "description": "The amount of carbohydrates in grams."
+      "name": "nutritional_info",
+      "schema": {
+        "type": "object",
+        "properties": {
+          "carbohydrates": {
+            "type": "number",
+            "description": "The amount of carbohydrates in grams."
+          },
+          "protein": {
+            "type": "number",
+            "description": "The amount of protein in grams."
+          },
+          "fat": {
+            "type": "number",
+            "description": "The amount of fat in grams."
+          },
+          "total_calories": {
+            "type": "number",
+            "description": "The total caloric content in kilocalories."
+          }
+        },
+        "required": [
+          "carbohydrates",
+          "protein",
+          "fat",
+          "total_calories"
+        ],
+        "additionalProperties": false
       },
-      "protein": {
-        "type": "number",
-        "description": "The amount of protein in grams."
-      },
-      "fat": {
-        "type": "number",
-        "description": "The amount of fat in grams."
-      },
-      "total_calories": {
-        "type": "number",
-        "description": "The total caloric content in kilocalories."
-      }
-    },
-    "required": [
-      "carbohydrates",
-      "protein",
-      "fat",
-      "total_calories"
-    ],
-    "additionalProperties": false
-  },
-  "strict": true
-}'
-    structured_output = c.assistant!
+      "strict": true
+    }'
+    @structured_output = c.assistant!
+
+    @g_carbs = @structured_output.fetch("carbohydrates")
+    @g_protein = @structured_output.fetch("protein")
+    @g_fat = @structured_output.fetch("fat")
+    @kcal = @structured_output.fetch("total_calories")
 
     render ({ :template => "macros/results" })
   end
